afa1c1a3621c4bef636dd3502c9e6dc96c2e5467
Lorenzo Chiantelli <salvatorebizuli@gmail.com>
2025-03-25 16:31:03 -0300

    Cria um arquivo sobre concatenação

---
+++ aula 02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    echo "Heloo World!<br>";
+
+    $nome = "Lorenzo";
+    $sobrenome = "Chiantelli";
+    $numero = 4;
+
+    echo $nome . " lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p>" . $nome . 
+    " " . $sobrenome . "</p>";
+?>
